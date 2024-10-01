a690ee80c72d76d74e59635f2bfaf48533ea0f70
Ryo-Sasaki <82487361+roy-aaissk@users.noreply.github.com>
2024-10-01 19:16:59 +0900

    Feature/01 (#52)
    * remove solove
    
    * add palindromenumber

--- algorithms/9.PalindromeNumber/palindromenumber.go
+++ algorithms/9.PalindromeNumber/palindromenumber.go
@@ -1,13 +1,10 @@
-package main
+package palindromenumber
 
 import (
 	"strconv"
 )
 
-func main() {
-}
-
-func isPalindrome(x int) bool {
+func IsPalindrome(x int) bool {
 	x_str := strconv.Itoa(x)
 	for i := 0; i < len(x_str); i++ {
 		if x_str[i] != x_str[len(x_str)-1-i] {
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/roy-aaissk/LeetCode
+module leet_code
 
-go 1.21.0
+go 1.23.1
