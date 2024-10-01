676dd5f7698dafd344e66ba8119aef6088544758
Ryo-Sasaki <82487361+roy-aaissk@users.noreply.github.com>
2024-10-01 18:23:14 +0900

    remove solove (#51)

--- solutions/2114. Maximum Number of Words Found in Sentences.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def mostWordsFound(self, sentences: List[str]) -> int:
-        max = 0
-        for i in range(len(sentences)):
-            target = sentences[i].count(' ')+ 1
-            if target > max:
-                max = target
-        return max
--- solutions/SQL/DailyLeadsandPartners.sql
+++
@@ -1,1 +0,0 @@
-SELECT date_id, make_name, Count(DISTINCT lead_id) AS unique_leads, Count( DISTINCT partner_id) AS unique_partners FROM DailySales GROUP BY date_id, make_name;
